0861b87a7ed5b8ac25fb10d2ca5c6f599d8efde9
Hariharan Jayashankar <hariharan.jayashankar@gmail.com>
2021-05-16 19:16:12 +0530

    Made coefficient same as paper

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ The package uses the [Rendahl (2017)](https://www.ihs.ac.at/publications/eco/es-
 
 Package was built because it seemed like there was no quick and dirty way of playing around with big macro models.
 
-Using the package is simple. All you need to do is define a function `F(Xf, X, Xl, ϵ, args)` which outputs the residuals of the equiblibrium equations of the model. `Xf` are supposed to be the one period forward variables, `X` the current variables, and `Xl` the lagged variables. `ϵ` should contain all the shocks of the model. `args` are optional arguments you may want to pass in like parameters the model needs.
+Using the package is simple. All you need to do is define a function `F(Xl, X, Xf, ϵ, args)` which outputs the residuals of the equiblibrium equations of the model. `Xf` are supposed to be the one period forward variables, `X` the current variables, and `Xl` the lagged variables. `ϵ` should contain all the shocks of the model. `args` are optional arguments you may want to pass in like parameters the model needs.
 
 
 # An Example - A simple RBC Model
@@ -14,7 +14,7 @@ Using the package is simple. All you need to do is define a function `F(Xf, X, X
 For example the `F` function for an RBC model might look like:
 
 ```julia
-function F(Xf, X, Xl, ϵ, params)
+function F(Xl, X, Xf, ϵ, params)
     #====================
     Xf = X_{t+1}
     X = X_t
--- src/solver.jl
+++ src/solver.jl
@@ -4,16 +4,16 @@ using ForwardDiff
 using LinearAlgebra
 using Plots
 
-struct solution
-    P::Matrix{Float}
-    Q::Matrix{Float} 
-    irf::Matrix{Float} 
-    xss::Vector{Float}
+mutable struct solution
+    F::Matrix{Float64}
+    Q::Matrix{Float64} 
+    irf::Matrix{Float64} 
+    xss::Vector{Float64}
 
-    A::Matrix{Float} 
-    B::Matrix{Float}
-    C::Matrix{Float}
-    E::Matrix{Float}
+    A::Matrix{Float64} 
+    B::Matrix{Float64}
+    C::Matrix{Float64}
+    E::Matrix{Float64}
 end
 
 
@@ -67,29 +67,29 @@ function solve_system(A, B, C, E, maxiter=1000, tol=1e-6)
     Solves for P and Q using Rehndal's Algorithm
     ==#
     
-    P0 = zero(A)
+    F0 = zero(A)
     S0 = zero(A)
     error = one(tol) + tol
     iter = 0
     
     while error > tol && iter <= maxiter
         
-        P1 = -(A * P0 + B) \ C
-        S1 = -(C * S0 + B) \ A
+        F1 = -(C * F0 + B) \ A
+        S1 = -(A * S0 + B) \ C
         
-        error = maximum(A * P1 * P1  + B * P1 + C)
+        error = maximum(C * F1 * F1  + B * F1 + A)
         
-        P0 .= P1
+        F0 .= F1
         S0 .= S1
         
         iter += 1
         
     end
     
-    XP = LinearAlgebra.eigen(P0)
+    XP = LinearAlgebra.eigen(F0)
     XS = LinearAlgebra.eigen(S0)
     
-    Q = -(A * P0 + B) \ E
+    Q = -(C * F0 + B) \ E
     
     if iter == maxiter
         println("Convergence Failed. Max Iterations Reached. Error: $error")
@@ -101,19 +101,19 @@ function solve_system(A, B, C, E, maxiter=1000, tol=1e-6)
         println("Convergence Successful!")
     end
     
-    return P0, Q
+    return F0, Q
 
     
 end
 
 
-function compute_irfs(P, Q, xss, timeperiods = 40)
+function compute_irfs(F, Q, xss, timeperiods = 40)
 
-    IRF = zeros(size(P, 1), timeperiods)
+    IRF = zeros(size(F, 1), timeperiods)
 
     IRF[:, 1] = Q
     for t in 2:timeperiods
-        IRF[:, t] = P * IRF[:, t-1]
+        IRF[:, t] = F * IRF[:, t-1]
     end
 
     IRF = IRF .+ xss
@@ -147,10 +147,10 @@ function solve(F, fargs; xinit, irf_timeperiods)
 
     xss = get_ss(F, xinit, fargs)
     A, B, C, E = rendahl_coeffs(F, xss, fargs)
-    P, Q = solve_system(A, B, C, E)
-    irf = compute_irfs(P, Q, xss, irf_timeperiods)
+    F, Q = solve_system(A, B, C, E)
+    irf = compute_irfs(F, Q, xss, irf_timeperiods)
 
-    out = solution(P, Q, irf, xss, A, B, C, E)
+    out = solution(F, Q, irf, xss, A, B, C, E)
 
     return out
     
